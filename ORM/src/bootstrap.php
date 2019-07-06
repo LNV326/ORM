9e1c1ce69f3f961d90f1baed5855e6f301d9f3c0
LNV326 <lnv326@gmail.com>
2019-07-06 19:06:24 +0300

    Change composer dependencies, add doctrine second level cache support.

--- ORM/src/bootstrap.php
+++ ORM/src/bootstrap.php
@@ -1,54 +1,106 @@
 <?php
 echo 'Bootstrap is started <br>';
-
 use Doctrine\ORM\Tools\Setup;
 use Doctrine\ORM\EntityManager;
 use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Cache\ArrayCache;
-use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
 use Doctrine\Common\Cache\MemcacheCache;
+use Doctrine\ORM\Mapping\Driver\AnnotationDriver;
+use Doctrine\ORM\Proxy\ProxyFactory;
+use Doctrine\Common\Cache\MemcachedCache;
+use Doctrine\ORM\Cache\RegionsConfiguration;
+use Doctrine\ORM\Cache\DefaultCacheFactory;
+use Doctrine\DBAL\Logging\DebugStack;
+use Doctrine\ORM\Cache\Logging\StatisticsCacheLogger;
 
-$paths = array( realpath(__DIR__."/../src/Entity") );
 $isDevMode = true;
+$isEnableLogging = true;
 
-// the connection configuration
+// The connection configuration
 $dbParams = array(
-		'driver' 	=> 'pdo_mysql',
-		'user'		=> 'sa1',
-		'password'	=> '123',
-		'dbname'	=> 'u11795_nfsko'
+    'driver' 	=> 'pdo_mysql',
+    'user'		=> 'root',
+    'password'	=> '123',
+    'dbname'	=> 'db_nfsko_190615'
 );
 
-if ($isDevMode == true) {
-	$cache = new \Doctrine\Common\Cache\ArrayCache;
+/**
+ * Main cache configuration
+ * 
+ * Use memory for dev-env.
+ * Use caching mechanism (Memcache for example) for prod-env.
+ * @see https://www.doctrine-project.org/projects/doctrine-orm/en/2.6/reference/caching.html
+ */
+if ($isDevMode) {
+    $cacheDriver = new ArrayCache;
 } else {
-	$cache = new MemcacheCache;
-	$memcache = new Memcache();
-	$memcache->connect('localhost', 11211);
-	$cache->setMemcache($memcache);
+    // Memcache (depricated)
+//     $memcache = new Memcache();
+//     $memcache->connect('localhost', 11211);
+//     $cacheDriver = new MemcacheCache();
+//     $cacheDriver->setMemcache($memcache);
+    
+    // Memcached
+    $memcached = new Memcached();
+    $memcached->addServer('localhost', 11211);
+    $cacheDriver = new MemcachedCache();
+    $cacheDriver->setMemcached($memcached);
 }
 
+/**
+ * Creating annotation driver for metadata.
+ */
 $reader = new AnnotationReader();
-$driver = new AnnotationDriver($reader, $paths);
+$paths = array( realpath(__DIR__."/../src/Entity") );
+$proxyDir = realpath(__DIR__."/../tmp/Proxy");
+$metadataDriver = new AnnotationDriver($reader, $paths);
 
-$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode);
-$config->setMetadataCacheImpl( $cache );
-$config->setQueryCacheImpl( $cache );
-$config->setMetadataDriverImpl( $driver );
-$config->setProxyDir( realpath(__DIR__."/../tmp/Proxy") );
+/**
+ * Creating ORM configuration.
+ * CacheDriver uses for: MetadataCache, QueryCache, ResultCache
+ * 
+ * @see https://www.doctrine-project.org/projects/doctrine-orm/en/2.6/reference/caching.html
+ * @var \Doctrine\ORM\Configuration $config
+ */
+$config = Setup::createAnnotationMetadataConfiguration($paths, $isDevMode, $proxyDir, $cacheDriver);
+$config->setMetadataDriverImpl( $metadataDriver );
 
-if ($isDevMode == true) {
-	$config->setAutoGenerateProxyClasses(true);
+
+if ($isDevMode) {
+    $config->setAutoGenerateProxyClasses(ProxyFactory::AUTOGENERATE_ALWAYS);
 } else {
-	$config->setAutoGenerateProxyClasses(false);
+    $config->setAutoGenerateProxyClasses(ProxyFactory::AUTOGENERATE_FILE_NOT_EXISTS);
+    
+    /**
+     * Creating second-level cache. It uses for save results of DB queries.
+     * @see https://www.doctrine-project.org/projects/doctrine-orm/en/2.6/reference/second-level-cache.html
+     */
+    $config->setSecondLevelCacheEnabled(true);
+    $cacheConfig = new RegionsConfiguration(); 
+    $factory = new DefaultCacheFactory($cacheConfig, $cacheDriver);    
+    $config->getSecondLevelCacheConfiguration()->setCacheFactory($factory);
 }
 
-$entityManager = EntityManager::create($dbParams, $config);
-$em = $entityManager;
+// Enable logging
+if ($isEnableLogging) {
+    // Enable common logger for SQL-queries
+    $config->setSQLLogger(new DebugStack());
+    // Enable logger for second-level cache (result cache)
+    if ($config->isSecondLevelCacheEnabled()) {
+        $logger = new StatisticsCacheLogger();
+        $config->getSecondLevelCacheConfiguration()->setCacheLogger($logger);
+    }
+}
+
+/**
+ * Creating Entity Manager
+ * @var \Doctrine\ORM\EntityManager $em
+ */
+$em = EntityManager::create($dbParams, $config);
 $em->getConfiguration()->addEntityNamespace('Entity', 'src/Entity');
 
 // This I had to add to support the Mysql enum type.
-$platform = $entityManager->getConnection()->getDatabasePlatform();
+$platform = $em->getConnection()->getDatabasePlatform();
 $platform->registerDoctrineTypeMapping('enum', 'string');
 
 echo 'Bootstrap is finished <br>';
